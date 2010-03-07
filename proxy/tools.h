f28f563a1c8585947238ad51e3217acf32f36652
consros <consros@5d840e5e-ac03-11dd-b9e7-8d5594d729c2>
2010-03-07 23:18:02 +0000

    Comments

--- proxy/tools.h
+++ proxy/tools.h
@@ -5,19 +5,47 @@
 #include <string>
 using namespace std;
 
-// to skip socket output which can be ignored
+/**
+ * Reads given socket until there is no data left inside.
+ * Used to skip socket output which can be ignored.
+ * @param  sockfd file descriptor of open socket.
+ * @param  out if provided read data will be printed to file (e.g. stdout).
+ * @return read data as string.
+ */
 string readSocket(int sockfd, FILE* out = NULL);
 
-// open socket connection to given host
+/**
+ * Opens socket connection to given host.
+ * @param  host name of host to connect to.
+ * @param  port port number to connect to.
+ * @return file descriptor of open socket.
+ */
 int connectToHost(const char *host, int port);
 
-// reads page returned by specified url (HTTP GET method)
+/**
+ * Sends specified HTTP GET request and returns server response page.
+ * @param  url url to open with all the parameters.
+ * @param  headerExt if provided header extension included in request.
+ * @return page returned by server formed as string.
+ */
 string getPageContentByGet(string url, string headerExt = "");
 
-// reads page returned by specified url (HTTP POST method)
+/**
+ * Sends specified HTTP POST request and returns server response page.
+ * @param  url url to open.
+ * @param  parameters parameters to send via POST.
+ * @param  headerExt if provided header extension included in request.
+ * @return page returned by server formed as string.
+ */
 string getPageContentByPost(string url, string parameters, string headerExt = "");
 
-// returns string contained between two strings
+/**
+ * Parses string and retured first string placed between two given strings.
+ * @param  text text to parse.
+ * @param  start string bounding start of searched pattern.
+ * @param  stop  string bounding stop  of searched pattern.
+ * @return string placed between two given strings.
+ */
 string findExpr(string text, string start, string stop);
 
 #endif
