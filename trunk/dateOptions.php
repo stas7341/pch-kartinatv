cfbbdae0dd019e24ed4b2af323b40b662d2eda43
consros <consros@5d840e5e-ac03-11dd-b9e7-8d5594d729c2>
2010-03-20 17:53:03 +0000

    Optimizations

--- trunk/dateOptions.php
+++ trunk/dateOptions.php
@@ -1,45 +1,41 @@
 <?php
+#############################################################################
+# Fast archive days navigation.                                             #
+#                                                                           #
+# Author: alexandr.dubovikov 2010                                           #
+# Author: consros 2010                                                      #
+#############################################################################
 
+require_once "pageTools.inc";
 require_once "settings.inc";
-require_once "ktvFunctions.inc";
-require_once "channelsParser.inc";
+require_once "tools.inc";
 
-# id transmitted as a part of ref parameter at the very end
-$id  = preg_replace('/.*\?id=/', '', $_GET['ref']);
-$currentTime = time() + (TIME_ZONE * 60 * 60);
+displayHtmlHeader("NMT Kartina.TV Archive");
 ?>
-<html>
-<head>
-    <title>Archive</title>
-    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
-    <?php displayCommonStyles(FONT_SIZE); ?>
-    <style type="text/css">
-        th { background-color: #005B95; width: 500px; }
-        td { background-color: #6d80a0; }
-    </style>
-</head>
-<body focusColor="#880000" focusText="#FFFFFF" >
-<div align="center">
-<table>
-<tr><th align="center">Kartina.TV Archive</th></tr>
+<style type="text/css">
+    th { background-color: #005B95; width: 500px; }
+    td { background-color: #6d80a0; }
+</style>
 <?php
-for ($i=0; $i < 12; $i++) {
-    $currentTime-=86400;
+displayHtmlBody();
+print '<table><tr><th align="center">' . LANG_DAYS_LIST . '</th></tr>';
 
-    $linkUrl  = "displayProgram.php?ref=" . $_GET['ref'];
-    $linkUrl .= "&id=" . $_GET['id'];
-    $linkUrl .= "&title=" . $_GET['title'];
-    $linkUrl .= "&number=" . $_GET['number'];
-    $linkUrl .= "&vid=" . $_GET['vid'];
-    $linkUrl .= "&archiveTime=" . $currentTime;
+$arcTime = time() + (TIME_ZONE * 60 * 60);
+$linkUrl  = "displayProgram.php?ref=" . $_GET['ref'];
+$linkUrl .= "&id="     . $_GET['id'];
+$linkUrl .= "&title="  . $_GET['title'];
+$linkUrl .= "&number=" . $_GET['number'];
+$linkUrl .= "&vid="    . $_GET['vid'];
 
-    $name = date("d.m.Y", $currentTime);
-    $name = "<a href=\"$linkUrl\" $linkExt>$name</a>";
+for ($i = 0; $i < 14; $i++) {
+    $arcTime -= 24 * 60 * 60;
+
+    $name = formatDate('d.m.Y', $arcTime);
+    $name = "<a href=\"$linkUrl&archiveTime=$arcTime\">$name</a>";
 
     print '<tr><td align="center">' . $name . "</td></tr>\n";
 }
+
+print '</table>';
+displayHtmlEnd();    
 ?>
-</table>
-</div>
-</body>
-</html>
